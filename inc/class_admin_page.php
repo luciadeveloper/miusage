a0b6c299510844f4e1066b1435e36a8edd810f39
lucia sanchez fraile <luciasanchezfraile@lucias-MacBook-Pro.local>
2021-02-06 23:47:46 +0100

    table format

--- inc/class_admin_page.php
+++ inc/class_admin_page.php
@@ -2,51 +2,41 @@
 
 if( ! defined( 'ABSPATH' ) ) exit;
 
-//require plugin_dir_path( __FILE__ ) . 'miusage_plugin.php';
-
-
 
 if ( !class_exists( 'Miusage_admin_page' ) ) {
     class Miusage_admin_page {
         public function __construct() {
             add_action( 'admin_menu', array($this,'miusage_admin_page')); 
             add_action("wp_ajax_miusage_data_print", array($this,"miusage_data_print"));
+            add_action( 'admin_head', array($this,"wp_enqueue_scripts") );
         }
+        public function wp_enqueue_scripts() {
+            wp_register_style( 'miusage_admin_page', plugins_url( '../src/admin_page.css', __FILE__ ), array(),  _S_VERSION );
+            wp_enqueue_style( 'miusage_admin_page');
+
+            wp_enqueue_script( 'admin_refresh', plugins_url( '../src/admin_refresh.js', __FILE__ ), array(), _S_VERSION, true );
+        }
+    
 
         public function miusage_admin_page() {
-            add_menu_page(
-               __( 'miusage_data', 'textdomain' ),
-               __( 'miusage_data','textdomain' ),
-               'manage_options',
-               'miusage_data',
-               array($this,'miusage_data_admin'),
-               'dashicons-format-aside', 
-           );
+            add_menu_page(__( 'miusage_data', 'textdomain' ),__( 'miusage_data','textdomain' ),'manage_options','miusage_data',array($this,'miusage_data_admin'),'dashicons-format-aside', );
         }
 
+        //for the ajax call to refresh data 
         public function miusage_data_print(){
             global $Miusage;
-            $data = $Miusage->print_miusage_data();
+            $data = $Miusage->print_miusage_table();
             print_r($data);
         }
 
         public function miusage_data_admin(){
             global $Miusage;
-            $data = $Miusage->print_miusage_data();
-            $button = '<div href="" id="button-refresh">Refresh</div>';  //translations!
-          
-            $html = '<div id="info">' . $data . '</div>' . $button;
+            $data = $Miusage->print_miusage_table();
+            $button = '<button href="" id="button-refresh">Refresh</button>';  //translations!
+            $loader = '<span id="loaderDiv" ></span>';
+            $html = '<div id="info">' . $data . '</div>' . $loader. $button;
             echo $html;
-            ?>  
-            <script>
-                jQuery(document).on('click', '#button-refresh', function(e){
-                    jQuery.ajax({url: "http://localhost:8000/wp-admin/admin-ajax.php?action=miusage_data_print", success: function(result){
-                        jQuery("#info").html(result);
-                        console.log(result);
-                    }});
-                });
-            </script>
-            <?php
+            
         }
     }
 }
