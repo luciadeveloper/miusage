caa8fd8fbb545c0e4a65b75ad6985a436b085417
Lucia Sanchez Fraile <luciasanchezfraile@lucias-MacBook-Pro.local>
2021-02-07 16:34:02 +0100

    console log deleted

--- src/admin_refresh.js
+++ src/admin_refresh.js
@@ -1,13 +1,15 @@
 jQuery( '#loaderDiv' ).hide();
-console.log(WPDomain.siteurl)
+
 jQuery( document ).on('click', '#button-refresh', function( e ){
     jQuery.ajax({
         type: 'POST',
         url: WPDomain.siteurl+'/wp-admin/admin-ajax.php?action=miusage_data_print_refresh',
+       
         beforeSend: function() {
             jQuery( '#loaderDiv' ).show();
             jQuery( '#button-refresh' ).hide();
         },
+       
         success: function( data ) {
             jQuery( '#loaderDiv' ).hide();
             jQuery( '#button-refresh' ).show();
